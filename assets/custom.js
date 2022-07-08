aa92f91e16850f134103ef57dc498aebb9369509
Tehman Sardar <tehmansardar@hotmail.com>
2022-07-09 01:46:50 +0500

    V1

---
+++ assets/custom.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict'
+    var lightbox = function () {
+        var lightboxVideo = GLightbox({
+            selector: '.glightbox'
+        });
+    };
+    lightbox();
+})()
